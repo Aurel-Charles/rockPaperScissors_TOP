af83e5f9b86a4bb4da0df1d30cd740ceccde7994
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-02 20:24:36 +0200

    add getComputerChoice variable
    clean the logic to get a random computer choice

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
+
 // Dertermined the computer Choice
-    // function to generate 3 random number (1 2 3)
-    // function that make each number equal to a choice (1 = rock ; 2 = paper ; 3 = scisoors)
-    //put the value inside a variable
-// Dertermined the human Choice
-// compare the computer with the human Choice
-// show the result
-
-
-
-// Create a new function named getComputerChoice. Game is played against the computer 
-
-function getRandomNumber(max) {
-  return Math.floor(Math.random() * max);  //get a ramdom number between 0 and "max" 
-}
-
 //transforme a number into. a string choice (1 > rock)
-function getRandomChoice(number) {
+function getRandomChoice() {
+    let number = (Math.floor(Math.random() * 3) + 1) //get a ramdom number between 0 and "max" 
+
     if (number === 1) {
         return "rock"
     }  
@@ -27,17 +15,14 @@ function getRandomChoice(number) {
     }
 }
 
+let getComputerChoice = getRandomChoice() // Create a new function named getComputerChoice with the choice. Game is played against the computer 
+
+console.log("The computer choosed: " + getComputerChoice);
+
+getComputerChoice = getRandomChoice()
+console.log("The computer choosed: " + getComputerChoice);
 
 
-
-let getComputerChoice
-
-getComputerChoice = getRandomNumber(3) + 1
-
-console.log(getComputerChoice);
-
-console.log(getRandomChoice(3));
-
-
-//console.log(getRandomNumber(3));
-// Expected output: 0, 1 or 2
+// Dertermined the human Choice
+// compare the computer with the human Choice
+// show the result
