0768566d8bae87c5b89b4881eeaf8238f8e46825
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-02 17:33:36 +0200

    Add index.html and script.js with basics

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dertermined the computer Choice
+// Dertermined the human Choice
+// compare the computer with the human Choice
+// show the result
+
+
+
+// Create a new function named getComputerChoice. Game is played against the computer 
+
