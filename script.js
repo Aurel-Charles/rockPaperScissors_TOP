702ac4139f439b48bac08ed76f55c0c1f48ec5ee
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-03 08:44:53 +0200

    Create new variables to store scores
    humanScore
    computerScore

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 // Dertermined the computer Choice
-//transforme a number into. a string choice (1 > rock)
+
 function getComputerChoice() {
     let number = (Math.floor(Math.random() * 3) + 1) //get a ramdom number between 0 and "max" 
 
     if (number === 1) {
-        return "rock"
+        return "rock" //transforme a number into. a string choice (1 > rock)
     }  
     if (number === 2) {
         return "paper"
@@ -15,12 +15,24 @@ function getComputerChoice() {
     }
 }
 
-// Create a new function named getComputerChoice with the choice. Game is played against the computer 
 
 console.log("The computer choosed: " + getComputerChoice());
 
 
 
 // Dertermined the human Choice
+
+function getHumanChoice() {
+    let askChoice =  window.prompt("Rock? Paper? or Scissors?") 
+    console.log("You chosed: " +  askChoice);
+    
+} 
+
+getHumanChoice()
+
 // compare the computer with the human Choice
+
+let humanScore = 0
+let computerScore = 0
+
 // show the result
