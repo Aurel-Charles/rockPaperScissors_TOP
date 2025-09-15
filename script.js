b1b856a1aa1d540d526184f2416ae4c1c8d71330
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-15 10:51:54 +0200

    Add logic on the Rock button
    when clicking => launch a playround and update score(cosole)

--- script.js
+++ script.js
@@ -17,13 +17,6 @@ function getComputerChoice() {
 
 
 
-function getHumanChoice() {
-    let Choice =  window.prompt("Rock? Paper? or Scissors?")
-    return Choice   
-} 
-
-
-
 
 
 
@@ -34,59 +27,11 @@ let computerScore = 0
 function playGame() {
     
     
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
-        
-        switch (humanChoice) {
-            case "rock":
-                if (humanChoice === computerChoice) {
-                    return "It's a tie, play again"
-                }
-                else if (humanChoice  && computerChoice === "paper" ) {
-                    return "You lost! Paper wrap the Rock "
-                }
-                else if (humanChoice  && computerChoice === "scissors" ) {
-                    return "You win!  Rock brakes Scissors"
-                }
-                
-                break;
-        
-            case "paper":
-                if (humanChoice === computerChoice) {
-                    return "It's a tie, play again"
-                }
-                else if (humanChoice && computerChoice === "rock" ) {
-                    return "You win! Paper wrap the Rock "
-                }
-                else if (humanChoice && computerChoice === "scissors" ) {
-                    return "You lost! Scissors cut Paper"
-                }
-                
-                break;
-            
-            case "scissors":
-                if (humanChoice === computerChoice) {
-                    return "It's a tie, play again"
-                }
-                else if (humanChoice && computerChoice === "rock" ) {
-                    return "You lost! Rock brakes Scissors"
-                }
-                else if (humanChoice && computerChoice === "paper" ) {
-                    return "You win! Scissors cut paper "
-                }
-                
-                break;
-        
-            default:
-                return "You typed something wrong"
-                break;
-        }
-    
-    } 
-    
     
     // compare and log humanChoice computerChoice  and the result with the function playRound()
     // Game start here
+
+
     
     const humanSelection = getHumanChoice();
     console.log("You chosed: " + humanSelection);
@@ -118,5 +63,103 @@ function playGame() {
 
 }
 
+////
+////
+////
 
-playGame()
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    
+    switch (humanChoice) {
+        case "rock":
+            if (humanChoice === computerChoice) {
+                return "It's a tie, play again"
+            }
+            else if (humanChoice  && computerChoice === "paper" ) {
+                return "You lost! Paper wrap the Rock "
+            }
+            else if (humanChoice  && computerChoice === "scissors" ) {
+                return "You win!  Rock brakes Scissors"
+            }
+            
+            break;
+    
+        case "paper":
+            if (humanChoice === computerChoice) {
+                return "It's a tie, play again"
+            }
+            else if (humanChoice && computerChoice === "rock" ) {
+                return "You win! Paper wrap the Rock "
+            }
+            else if (humanChoice && computerChoice === "scissors" ) {
+                return "You lost! Scissors cut Paper"
+            }
+            
+            break;
+        
+        case "scissors":
+            if (humanChoice === computerChoice) {
+                return "It's a tie, play again"
+            }
+            else if (humanChoice && computerChoice === "rock" ) {
+                return "You lost! Rock brakes Scissors"
+            }
+            else if (humanChoice && computerChoice === "paper" ) {
+                return "You win! Scissors cut paper "
+            }
+            
+            break;
+    
+        default:
+            return "You typed something wrong"
+            break;
+    }
+
+} 
+
+function updateScore(roundResult) {
+    
+    
+    // increment score based on round result
+    
+    let win = roundResult.startsWith("You win!") // determine if you win or not
+    let tie = roundResult.startsWith("It's a tie")// determine if it's a tie
+    
+    if (tie) {
+    }
+    else if (win) {
+     humanScore++    
+    }
+    else {
+      computerScore++
+    }
+    
+    let totalScore = "Your score is: " + humanScore + ", Computer score is: " + computerScore
+    console.log(totalScore);
+}
+    
+
+
+
+
+const btnRock = document.querySelector('#rock')
+btnRock.addEventListener('click', ()=>  {
+    const computerSelection = getComputerChoice();
+    let roundResult = playRound("rock", computerSelection)
+    console.log(roundResult);
+    updateScore(roundResult);
+
+})
+
+const btnPaper = document.querySelector('#paper')
+
+const btnScissors = document.querySelector('#scissors')
+
+
+function getHumanChoice() {
+    let Choice =  window.prompt("Rock? Paper? or Scissors?")
+    return Choice   
+} 
+
+
